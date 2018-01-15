2b479f9ae4a48e8dc137a3759616aecec6f69b4b
Jeffrey Kantor <Kantor.1@nd.edu>
2018-01-15 15:24:19 -0500

    starting files

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='pcPID',
+      version='0.1.0.dev1',
+      description='A PID control class for process control simulations.',
+      author='Jeffrey C. Kantor',
+      py_modules=['pcPID'],
+      )
